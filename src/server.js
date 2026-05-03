c8b40406d73781cda5ca036aeb26941244d9a255
aplus <beinghuman5320@gmail.com>
2026-05-03 22:14:43 +0600

    app remove

---
+++ src/server.js
@@ -0,0 +1,45 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.routes.js";
+import contactRoutes from "./routes/contact.routes.js";
+
+const app = express();
+
+/* ================= DATABASE ================= */
+connectDB();
+
+/* ================= MIDDLEWARE ================= */
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://medicaladmin-ashen.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+
+/* ================= ROUTES ================= */
+app.use("/api/products", productRoutes);
+app.use("/api/contact", contactRoutes);
+
+/* ================= ERROR HANDLING ================= */
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION 💥", err);
+});
+
+/* ================= START SERVER ================= */
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
+});
