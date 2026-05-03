cfd7847a09e832938f504196ca314b9132e70940
aplus <beinghuman5320@gmail.com>
2026-05-03 22:22:51 +0600

    API is running

--- src/server.js
+++ src/server.js
@@ -40,6 +40,10 @@ process.on("unhandledRejection", (err) => {
 /* ================= START SERVER ================= */
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} 🚀`);
 });
